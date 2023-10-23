12a66f2c7e9b8c286cdcf28a8923c15c40d4ee6c
olivejoys <olivejoy@sheridancollege.ca>
2023-10-23 10:51:25 -0400

    Added Enum method to UnoOnline class.

--- src/arithmetic/Arithmetic.java
+++ src/arithmetic/Arithmetic.java
@@ -1,44 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package arithmetic;
 
-
 import java.util.Scanner;
-import static java.time.Clock.system;
 
-/** This class calls the method to perform 
- * arithmetic operations based on user input
- * execute the code check the output
- * @author sivagamasrinivasan
- * 
- */
-public class Arithmetic 
-{
+public class Arithmetic {
+    public static void main(String[] args) {
+        ArithmeticBase r = new ArithmeticBase();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) 
-    {
-       
-        ArithmeticBase r= new ArithmeticBase();
-        Scanner in= new Scanner(System.in);
-        int n= in.nextInt();
-        int m= in.nextInt();
-        double result = r.calculate(m,n);
-        System.out.println("result :" +result); 
+        // Get the operation input from the user
+        System.out.println("Enter arithmetic operation to Perform (PLUS, MINUS, TIMES, DIVIDE): ");
+        String operationInput = in.next();
+        ArithmeticOperation operation = ArithmeticOperation.valueOf(operationInput.toUpperCase());
+
+        double result = r.calculate(m, n, operation);
+        System.out.println("Result: " + result);
     }
-        enum ArithmeticOperation {
+}
+
+enum ArithmeticOperation {
     PLUS,
     MINUS,
     TIMES,
     DIVIDE
 }
-
-    
-    }
-
-
